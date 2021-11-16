3e02df2a852699b3118d6b3ae1d410e556d1e489
jessica_b <jessica.francavilla86@gmail.com>
2021-11-15 22:40:19 -0300

    add styled table with conditionals

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    nombre: "jessica",
+    items: [
+      {
+        id: 1,
+        nombre: fruta,
+        precio: $12,
+        stock: true,
+      },
+      {
+        id: 2,
+        nombre: verdura,
+        precio: $10,
+        stock: false,
+      },
+    ],
+  },
+});
