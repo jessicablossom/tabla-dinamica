8adbc8dc1f26e45a1cc8bb9c4103127327a9776c
jessica_b <jessica.francavilla86@gmail.com>
2021-11-16 10:44:26 -0300

    add item card, css styles

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 var app = new Vue({
   el: "#app",
   data: {
-    nombre: "jessica",
     items: [
       {
         id: 1,
-        nombre: fruta,
-        precio: $12,
+        nombre: "fruta",
+        precio: "$12",
         stock: true,
       },
       {
         id: 2,
-        nombre: verdura,
-        precio: $10,
+        nombre: "verdura",
+        precio: "$10",
         stock: false,
+      },
+      {
+        id: 3,
+        nombre: "Agua",
+        precio: "$10",
+        stock: true,
       },
     ],
   },
